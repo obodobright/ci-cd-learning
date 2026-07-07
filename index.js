184a45cdb6ae9226532bb0c952822eb10bf36b4a
bright obodo <bright.obodo@moneyxbank.com.ng>
2026-07-07 22:27:47 +0100

    New: Added pipeline yaml file for github actions pratice

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("HELLO DEVOPS");
+
+console.log("I AM LEARNING CI/CD WITH GITHUB ACTIONS");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Let's tesst the application now");
+
+setTimeout(() => console.log("Testing the application already"), 3000);
+
+console.log("All test carried");
